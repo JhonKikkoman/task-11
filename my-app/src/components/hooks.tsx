2c3ce2303cacc3660631fc7b230e194f6db1866b
Piskun Ivan <tw1st3r111@mail.ru>
2023-12-27 23:07:24 +0300

    task11

--- my-app/src/components/hooks.tsx
+++ my-app/src/components/hooks.tsx
@@ -3,6 +3,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 
-// Используйте во всем приложении вместо обычных `useDispatch` и `useSelector`
+//  вместо обычных `useDispatch` и `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
